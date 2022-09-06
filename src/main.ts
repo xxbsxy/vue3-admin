62310ecd9db7ea904733610108abc445c7fae02e
陈桂东 <9542762+cgd-cgd@user.noreply.gitee.com>
2022-09-06 16:49:35 +0800

    commit

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ import './api/request/request'
 import 'element-plus/theme-chalk/el-loading.css'
 import 'element-plus/theme-chalk/el-message.css'
 import 'element-plus/theme-chalk/el-message-box.css'
-
-
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate);
 createApp(App).use(pinia).use(router).mount('#app')
